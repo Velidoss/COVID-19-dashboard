717ff245a6b0b4f813165dbb39b858f2d7bd3fd2
Velidoss <velidoss11@gmail.com>
2020-12-09 10:00:33 +0200

    feat: added eslint configuration

--- covid-dashboard/src/components/GlobalCases/GlobalCases.jsx
+++ covid-dashboard/src/components/GlobalCases/GlobalCases.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import style from './GlobalCases.module.scss';
 
-const GlobalCases = () => {
-  return (
-    <div className={style.GlobalCases}>
-      <p className={style.GlobalCases__text}>Global cases</p>
-      <p className={style.GlobalCases__number}>3564345</p>
-    </div>
-  )
-}
+const GlobalCases = () => (
+  <div className={style.GlobalCases}>
+    <p className={style.GlobalCases__text}>Global cases</p>
+    <p className={style.GlobalCases__number}>3564345</p>
+  </div>
+);
 
-export default GlobalCases
+export default GlobalCases;

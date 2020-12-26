f177b0533cdc6ca8dc73cbe9b35f3bba068b6bc9
Velidoss <velidoss11@gmail.com>
2020-12-26 23:09:04 +0200

    feat: added credits

--- covid-dashboard/src/components/Header/Header.jsx
+++ covid-dashboard/src/components/Header/Header.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import style from './Header.module.scss';
 import Search from './Search/Search';
 import Menu from './Menu/Menu';
+import gitIcon from '../../assets/svg/github-square-brands.svg';
+import schoolImg from '../../assets/img/rs_school_js.svg';
 
 const Header = () => (
   <header className={style.title}>
     <span className={style.titleText}>Covid dashboard</span>
+    <div className={style.title__credits}>
+      <a className={style.title__credits_author} href="https://github.com/Velidoss"><img src={gitIcon} alt="git" /></a>
+      <a className={style.title__credits_school} href="https://rs.school/js/"><img src={schoolImg} alt="rs-school" /></a>
+    </div>
     <Search />
     <Menu />
   </header>

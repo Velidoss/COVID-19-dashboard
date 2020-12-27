d0e64c43bbc4b50f5fb05c0eb4f236fecc95b0b8
Velidoss <velidoss11@gmail.com>
2020-12-27 14:48:16 +0200

    added adaptive collapse menu

--- covid-dashboard/src/components/Header/Header.jsx
+++ covid-dashboard/src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import style from './Header.module.scss';
-import Search from './Search/Search';
 import Menu from './Menu/Menu';
 import gitIcon from '../../assets/svg/github-square-brands.svg';
 import schoolImg from '../../assets/img/rs_school_js.svg';
@@ -13,7 +12,6 @@ const Header = () => (
       <a className={style.title__credits_author} href="https://github.com/Velidoss"><img src={gitIcon} alt="git" /></a>
       <a className={style.title__credits_school} href="https://rs.school/js/"><img src={schoolImg} alt="rs-school" /></a>
     </div>
-    <Search />
     <Menu />
   </header>
 );
--- covid-dashboard/src/components/Header/Menu/Menu.jsx
+++ covid-dashboard/src/components/Header/Menu/Menu.jsx
@@ -1,9 +1,14 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, {
+  useContext, useRef, useState, useEffect,
+} from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import style from './Menu.module.scss';
 import GlobalContext from '../../../store/GlobalContext';
 import SelectQuantities from './SelectQuantities/SelectQuantities';
 import contentConstants from '../../../constants/contentConstants';
+import Search from '../Search/Search';
+import menuIcon from '../../../assets/svg/bars-solid.svg';
+import useClickOutside from '../../../customHooks/useClickOutside';
 
 const Menu = () => {
   const {
@@ -13,7 +18,10 @@ const Menu = () => {
   const { locations } = contentConstants;
 
   const [nextLocation, setnextLocation] = useState(locations.main);
+  const [openMenu, toggleOpenMenu] = useState(false);
   const location = useLocation();
+  const menuRef = useRef();
+  useClickOutside(menuRef, () => toggleOpenMenu(!openMenu));
 
   useEffect(() => {
     if (location.pathname === '/') {
@@ -26,6 +34,7 @@ const Menu = () => {
   return (
     <div className={style.menu}>
       <div className={style.menu__items}>
+        <Search />
         <SelectQuantities
           selected={state.contentConfig.quantities}
           setContentConfig={setContentConfig}
@@ -42,6 +51,36 @@ const Menu = () => {
             )
             : null
         }
+      </div>
+      {
+        openMenu
+          ? (
+            <div ref={menuRef} className={style.menu__collapse}>
+              <Search />
+              <SelectQuantities
+                selected={state.contentConfig.quantities}
+                setContentConfig={setContentConfig}
+              />
+              {
+                nextLocation
+                  ? (
+                    <NavLink
+                      className={style.menu__collapse_link}
+                      to={nextLocation.to}
+                    >
+                      {nextLocation.name}
+                    </NavLink>
+                  )
+                  : null
+              }
+            </div>
+          )
+          : null
+      }
+      <div className={style.menu__open}>
+        <button onClick={() => toggleOpenMenu(!openMenu)} type="button" className={style.menu__open_button}>
+          <img className={style.menu__open_button_img} src={menuIcon} alt="openMenu" />
+        </button>
       </div>
     </div>
   );
